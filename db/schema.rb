c59dcd286f063064ef9c19b13bbf8f478743b1b2
ramesh-exalogic <ramesh.kumar@exalogic.co>
2024-10-30 11:01:27 +0530

    Added roles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_29_061032) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_30_052841) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,12 +52,19 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_29_061032) do
     t.integer "leave_type_id"
   end
 
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role", default: 0
+    t.integer "role_id"
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
